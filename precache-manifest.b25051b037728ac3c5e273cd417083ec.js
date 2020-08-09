cd5060175844124a4dccc5da2048f7429d2e7d21
daniel-sudz <daniel@sudzilouski.com>
2020-08-09 19:07:03 +0000

    Deploying to gh-pages from  @ 9d921a4f7e7295198b67e07756543ace186a5402 🚀

---
+++ precache-manifest.b25051b037728ac3c5e273cd417083ec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4c14a554d041ed30404485fe35a1972",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e3689f287fe7a7e784a4",
+    "url": "/static/css/main.46b4db55.chunk.css"
+  },
+  {
+    "revision": "4688aba86fd06de8fd78",
+    "url": "/static/js/2.e21c45c7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e21c45c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3689f287fe7a7e784a4",
+    "url": "/static/js/main.eb10dc4b.chunk.js"
+  },
+  {
+    "revision": "caf2a04453df4bace4e4",
+    "url": "/static/js/runtime-main.009173a0.js"
+  }
+]);
